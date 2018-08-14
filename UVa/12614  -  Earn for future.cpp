fcb49be45a695bdfa4501bf6528dfcca5efc75a3
ahbarif <ahbarif@outlook.com>
2018-08-14 22:43:39 +0600

    Create 12614  -  Earn for future.cpp

---
+++ UVa/12614  -  Earn for future.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+  //  freopen("in.txt", "r", stdin);
+
+    int t;
+
+    scanf("%d", &t);
+
+    for(int i = 1; i<=t; i++) {
+
+            int n, x, mx = 0;
+
+            scanf("%d", &n);
+
+            while(n--){
+                scanf("%d", &x);
+                mx = max(x, mx);
+            }
+
+            printf("Case %d: %d\n", i, mx);
+
+    }
+
+
+    return 0;
+}
